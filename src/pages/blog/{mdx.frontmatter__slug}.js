343175d7ed4a8f80e496bae5579304e75472d668
RazorHollow <rob@razorhollow.com>
2023-01-30 14:52:35 -0500

    dynamically generate blog pages

---
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+
+const BlogPost = (props) => {
+
+  console.log(props)
+  return (
+    <Layout pageTitle="Super Cool Blog Posts">
+      <p>My blog post contents will go here (eventually).</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title="Super Cool Blog Posts" />
+
+export default BlogPost
